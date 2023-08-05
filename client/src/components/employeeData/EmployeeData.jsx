83ff0bf09dbc733fe66b5ac3f278ccf781d06e99
whodivyanshu <divyanshulohar2002@gmail.com>
2023-08-05 18:25:45 +0530

    map update

--- client/src/components/employeeData/EmployeeData.jsx
+++ client/src/components/employeeData/EmployeeData.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Styles from "./employeeData.module.css";
 import { Box, Button } from '@mui/material';
 import Map from '../map/Map';
@@ -20,10 +20,7 @@ const EmployeeData = (props) => {
     p: 4,
 };
 
-const iconn = icon.icon({
-  iconUrl: "https://img.icons8.com/ios-filled/50/FF0000/visit.png",
-  iconSize: [30, 30]
-})
+
 
 const [Name, setName] = useState(props.name)
 const [updateDisable, setUpdateDisable] = useState(true);
@@ -32,6 +29,29 @@ const [Department, setDepartment] = useState(props.department)
 const [Status, setStatus] = useState(props.status)
 const [Address, setAddress] = useState(props.address)
 const [id, setId] = useState(props.idd)
+const [lat, setLat] = useState(0);
+const [lon, setLon] = useState(0);
+const iconn = icon.icon({
+  iconUrl: "https://img.icons8.com/ios-filled/50/FF0000/visit.png",
+  iconSize: [30, 30]
+})
+
+const addresss = `https://geocode.maps.co/search?q=${Address}`;
+
+useEffect(() => {
+  fetch(addresss)
+    .then((response) => response.json())
+    .then((data) => {
+      setLat(data[0].lat);
+      setLon(data[0].lon);
+      console.log(data[0].lat, data[0].lon);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}, [addresss]);
+
+
 
 
 
@@ -111,17 +131,7 @@ const [id, setId] = useState(props.idd)
             <div className={Styles.right} >
               <h1>Location</h1>
               <div className={Styles.location} >
-              <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "100%"}} >
-  <TileLayer 
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=O3hyGb236OQ11l0lXp7d"
-  />
-  <Marker position={[51.505, -0.09]} icon={iconn} >
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+              <Map lat={lat} lon={lon} />
               </div>
             </div>
         </Box>
--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import { CircleMarker, MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import "leaflet/dist/leaflet.css"
-import icon from "leaflet"
-const Map = () => {
-    const iconn = icon.icon({
-        iconUrl: "https://img.icons8.com/ios-filled/50/FF0000/visit.png",
-        iconSize: [30, 30]
-        })
-  return (
-    <div>              <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "100%"}} >
-    <TileLayer 
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=O3hyGb236OQ11l0lXp7d"
-    />
-    <Marker position={[51.505, -0.09]} icon={iconn}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer></div>
-  )
-}
+import L from "leaflet";
 
-export default Map
+const ChangeView = ({ center, zoom }) => {
+  const map = useMap();
+  useEffect(() => {
+    if (center[0] !== 0 && center[1] !== 0) {
+      map.setView(center, zoom);
+    }
+  }, [center, zoom]);
+
+  return null; // Add a return statement for the component
+};
+
+const Map = ({ lat, lon }) => {
+  const icon = L.icon({
+    iconUrl: "https://img.icons8.com/ios-filled/50/FF0000/visit.png",
+    iconSize: [30, 30]
+  });
+
+  return (
+    <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={false} style={{ width: "100%", height: "100%" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=O3hyGb236OQ11l0lXp7d"
+      />
+      <Marker position={[lat, lon]} icon={icon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      <ChangeView center={[lat, lon]} zoom={16} />
+    </MapContainer>
+  );
+};
+export default Map;
